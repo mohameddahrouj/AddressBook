efdcda56c9b13a70b4ba3fad03f06cb8ef025dff
Mohamed Dahrouj <dahrouj.m@gmail.com>
2018-02-08 12:32:51 -0500

    Root path points to index

---
+++ src/main/java/lab/MyWebMvcConfig.java
@@ -0,0 +1,22 @@
+package lab;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebMvcConfig {
+
+    @Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                // forward main index.html
+                registry.addViewController("/").setViewName("forward:/index.html");
+            }
+        };
+    }
+
+}
